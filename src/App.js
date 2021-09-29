9d118a98cfaf94bb8936225900524ee3145972b4
Danil <panarinbooking@gmail.com>
2021-09-29 16:48:41 +0300

    layout-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+      <div className="headerLeft">
+        <img width={40} height={40} src="/images/logo.png" alt="logo" />
+        <div className="headerInfo">
+          <h3 style={{textTransform: 'uppercase'}}>music store</h3>
+          <p style={{opacity: '.5'}}>The most delicious music</p>
+        </div>
+      </div>
+        <ul className="headerRight">
+          <li>
+            <img width={18} height={18} src="/images/cart.svg" alt="cart" />
+            <span>1205 сом.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/images/user.svg" alt="user" />
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1 style={{marginBottom: '40px'}}>All Music</h1>
+
+        <div className="card">
+          <img width={133} height={112} src="/images/music/1.jpg" alt="Music" />
+          <h5>
+            Виниловый Альбом
+            Indie Music 1986
+          </h5>
+          <div className="cardBottom">
+          <div className="cardPrice">
+            <span>Цена:</span>
+            <b>12 999 сом.</b>
+          </div>
+          <button className="button">
+            <img width={11} height={11} src="/images/plus.svg" alt="plus"/>
+          </button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
